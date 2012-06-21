46c30bca36b81555086002afbb276105dc904e34
Anton Ageev <antage@gmail.com>
2012-06-21 08:57:57 +0400

    Added CLI flags for host and port

--- main.go
+++ main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
-    "runtime"
+	"runtime"
 )
 
 func collect(w http.ResponseWriter, r *http.Request) {
-    log.Println(r)
+	log.Println(r)
 }
 
+var host = flag.String("h", "127.0.0.1", "host address (default 127.0.0.1)")
+var port = flag.Int("p", 9090, "port (default 9090)")
+
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())
-    http.HandleFunc("/collect", collect)
-    http.Handle("/favicon.ico", http.NotFoundHandler())
-    http.Handle("/", http.FileServer(http.Dir("assets")))
-	err := http.ListenAndServe(":9090", nil)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	flag.Parse()
+	http.HandleFunc("/collect", collect)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.Handle("/", http.FileServer(http.Dir("assets")))
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", *host, *port), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
