dca7df2fa25a22851a0ce3da8920c098143394a2
Anton Ageev <antage@gmail.com>
2012-06-25 07:43:26 +0400

    Revert "Fixed memory leaking?". The memory leak was in net/http module.
    This reverts commit 5374d3452615783a91c3c3044b7a187ff5381285.

--- updater.go
+++ updater.go
@@ -7,42 +7,40 @@ import (
 	"net/http"
 )
 
-func update(r *http.Request) {
-	log.Println(r)
-	s := ring.Current()
-
-	s.lock.Lock()
-	defer s.lock.Unlock()
-
-	s.requests++
-	b, err := strconv.ParseUint(r.Header.Get("X-Bytes-Sent"), 10, 64)
-	if err == nil {
-		s.bytes += b
-
-		referer := normalizeReferer(r.Header.Get("Referer"))
-		refererId := refererMapper.NameToId(referer)
-		if len(referer) > 0 {
-			if cb, ok := s.bytesByReferer[refererId]; ok {
-				s.bytesByReferer[refererId] = cb + b
-			} else {
-				s.bytesByReferer[refererId] = b
-			}
-		}
-
-		path := fmt.Sprintf("%s:%s", r.FormValue("bucket"), r.FormValue("uri"))
-		pathId := pathMapper.NameToId(path)
-		if len(path) > 0 {
-			if cb, ok := s.bytesByPath[pathId]; ok {
-				s.bytesByPath[pathId] = cb + b
-			} else {
-				s.bytesByPath[pathId] = b
-			}
-		}
-	}
-}
-
 func updater(source chan *http.Request) {
 	for r := range source {
-		update(r)
+		func() {
+			log.Println(r)
+			s := ring.Current()
+
+			s.lock.Lock()
+			defer s.lock.Unlock()
+
+			s.requests++
+			b, err := strconv.ParseUint(r.Header.Get("X-Bytes-Sent"), 10, 64)
+			if err == nil {
+				s.bytes += b
+
+				referer := normalizeReferer(r.Header.Get("Referer"))
+				refererId := refererMapper.NameToId(referer)
+				if len(referer) > 0 {
+					if cb, ok := s.bytesByReferer[refererId]; ok {
+						s.bytesByReferer[refererId] = cb + b
+					} else {
+						s.bytesByReferer[refererId] = b
+					}
+				}
+
+				path := fmt.Sprintf("%s:%s", r.FormValue("bucket"), r.FormValue("uri"))
+				pathId := pathMapper.NameToId(path)
+				if len(path) > 0 {
+					if cb, ok := s.bytesByPath[pathId]; ok {
+						s.bytesByPath[pathId] = cb + b
+					} else {
+						s.bytesByPath[pathId] = b
+					}
+				}
+			}
+		}()
 	}
 }
